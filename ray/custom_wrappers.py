63de3a9535a4f17416542d8892000f3885244f48
Oscar Sellors <omsellors98@gmail.com>
2025-05-28 07:35:41 +0100

    Experimented with Ray
    Self-play setup seems doable, however  complications with wrapping the environment using Diambra's interface suggests that I stick with SB3 for this thesis.

--- ray/custom_wrappers.py
+++ ray/custom_wrappers.py
@@ -1,17 +1,21 @@
 import gymnasium as gym
-from stable_baselines3.common.vec_env import VecEnv, VecEnvWrapper
+from ray.rllib.env.vector_env import VectorEnv, VectorEnvWrapper
 import numpy as np
 
-class VecEnvMDTransferWrapper(VecEnvWrapper):
+class VecEnvMDTransferWrapper(VectorEnvWrapper):
     '''
     VectorEnv Multi-Discrete Transfer Wrapper
     Necessary for wrapping SubprocVecEnvs during distributed training
     '''
-    def __init__(self, venv: VecEnv, stack_frames: int, no_move_idx: int = 0, no_attack_idx: int = 0):
-        super().__init__(venv=venv)
+    def __init__(self, venv: VectorEnv, stack_frames: int, no_move_idx: int = 0, no_attack_idx: int = 0):
+        super().__init__(
+            observation_space=venv.observation_space,
+            action_space=venv.action_space,
+            num_envs=venv.num_envs
+        )
         self.venv = venv
-        self.valid_moves = venv.action_space.nvec[0]
-        self.valid_attacks = venv.action_space.nvec[1]
+        self.valid_moves = venv.action_space["agent_0"].nvec[0]
+        self.valid_attacks = venv.action_space["agent_0"].nvec[1]
         self.action_space = gym.spaces.MultiDiscrete([9, 11]) # 11 attacks is the largest possible action space (kof)
         self.no_move_idx = no_move_idx
         self.no_attack_idx = no_attack_idx
@@ -21,35 +25,31 @@ class VecEnvMDTransferWrapper(VecEnvWrapper):
         else:
             self.image_space_keys = ["frame"]
 
-    def reset(self) -> np.ndarray:
-        obs = self.venv.reset()
+    def vector_reset(self):
+        obs = self.venv.vector_reset()
+        return obs["frame"]
+    
+    def reset_at(self, index):
+        obs = self.venv.reset_at(index)
         return obs["frame"]
 
-    def step_async(self, actions: np.ndarray) -> None:
-        '''
-        Checks if chosen actions are within the underlying venv's valid move list.
-        Replaces any that do not with no-op.
-        '''
-        actions = np.array([
-            (move if move < self.valid_moves else self.no_move_idx,
-            attack if attack < self.valid_attacks else self.no_attack_idx)
-            for move, attack in actions
-        ])
-        self.venv.step_async(actions)
+    def vector_step(self, actions):
+        obs, reward, dones, info = self.venv.vector_step(actions)
+        return obs["frame"], reward, dones, info
 
-    def step_wait(self):
-        obs, reward, done, info = self.venv.step_wait()
-        return obs["frame"], reward, done, info
-
-class VecEnvDiscreteTransferWrapper(VecEnvWrapper):
+class VecEnvDiscreteTransferWrapper(VectorEnvWrapper):
     '''
     VectorEnv Discrete Transfer Wrapper
     Necessary for wrapping SubprocVecEnvs during distributed training
     '''
-    def __init__(self, venv: VecEnv, stack_frames: int, no_op_idx: int = 0):
-        super().__init__(venv=venv)
+    def __init__(self, venv: VectorEnv, stack_frames: int, no_op_idx: int = 0):
+        super().__init__(
+            observation_space=venv.observation_space,
+            action_space=venv.action_space,
+            num_envs=venv.num_envs
+        )
         self.venv = venv
-        self.valid_actions = venv.action_space.n
+        self.valid_actions = venv.action_space["agent_0"].n
         self.action_space = gym.spaces.Discrete(19) # 19 actions is the largest possible action space (kof)
         self.no_op_idx = no_op_idx
         self.observation_space = gym.spaces.Box(0, 255, (84, 84, stack_frames), np.uint8)
@@ -58,29 +58,21 @@ class VecEnvDiscreteTransferWrapper(VecEnvWrapper):
         else:
             self.image_space_keys = ["frame"]
 
-    def reset(self) -> np.ndarray:
-        obs = self.venv.reset()
+    def vector_reset(self):
+        obs = self.venv.vector_reset()
+        return obs["frame"]
+    
+    def reset_at(self, index):
+        obs = self.venv.reset_at(index)
         return obs["frame"]
 
-    def step_async(self, actions: np.ndarray) -> None:
-        '''
-        Checks if chosen actions are within the underlying venv's valid move list.
-        Replaces any that do not with no-op.
-        '''
-        actions = np.array([
-            action if action < self.valid_actions else self.no_op_idx
-            for action in actions
-        ])
-        self.venv.step_async(actions)
-
-    def step_wait(self):
-        obs, reward, done, info = self.venv.step_wait()
-        return obs["frame"], reward, done, info
+    def vector_step(self, actions):
+        obs, reward, dones, info = self.venv.vector_step(actions)
+        return obs["frame"], reward, dones, info
 
 class MDTransferWrapper(gym.Wrapper):
     def __init__(self, env, stack_frames: int, no_move_idx: int = 0, no_attack_idx: int = 0):
         super().__init__(env)
-        self.env = env
         self.valid_moves = env.action_space.nvec[0]
         self.valid_attacks = env.action_space.nvec[1]
         self.action_space = gym.spaces.MultiDiscrete([9, 11]) # 11 actions is the largest possible action space (kof)
@@ -116,8 +108,7 @@ class MDTransferWrapper(gym.Wrapper):
 class DiscreteTransferWrapper(gym.Wrapper):
     def __init__(self, env, stack_frames: int, no_op_idx: int = 0):
         super().__init__(env)
-        self.env = env
-        self.valid_actions = env.action_space.n
+        self.valid_actions = env.action_space["agent_0"].n
         self.action_space = gym.spaces.Discrete(19) # 19 actions is the largest possible action space (kof)
         self.no_op_idx = no_op_idx
         self.observation_space = gym.spaces.Box(0, 255, (84, 84, stack_frames), np.uint8)
--- ray/self_play_ppo.py
+++ ray/self_play_ppo.py
@@ -5,16 +5,25 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
-from diambra.arena import SpaceTypes, EnvironmentSettings, WrappersSettings, load_settings_flat_dict
+from diambra.arena import SpaceTypes, EnvironmentSettingsMultiAgent, WrappersSettings, load_settings_flat_dict
 from diambra.arena.ray_rllib.make_ray_env import DiambraArena, preprocess_ray_config
 from ray.rllib.algorithms.ppo import PPO
 from ray.tune.logger import pretty_print
+from ray.tune.registry import register_env
 
 import torch
-# import custom_wrappers
-import sb3.utils as utils
+import custom_wrappers
+import utils
 
-def main(policy_cfg: str, settings_cfg: str, train_id: str | None, char_transfer: bool):
+# diambra run -g -s 9 python ray/self_play_ppo.py --policyCfg config_files/self-play-ppo.yaml --settingsCfg config_files/self-play-settings.yaml --trainID _
+
+def env_creator(env_config):
+    env = custom_wrappers.DiscreteTransferWrapper(DiambraArena(env_config), stack_frames=4)
+    print(f"Observation Space: {env.observation_space}")
+    print(f"Action Space: {env.action_space}")
+    return env
+
+def main(policy_cfg: str, settings_cfg: str, train_id: str):
     # Game IDs
     game_ids = [
         "sfiii3n",
@@ -24,7 +33,7 @@ def main(policy_cfg: str, settings_cfg: str, train_id: str | None, char_transfer
     ]
     
     if train_id not in game_ids:
-        train_id = None
+        train_id = game_ids[0]
 
     # Device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -61,40 +70,26 @@ def main(policy_cfg: str, settings_cfg: str, train_id: str | None, char_transfer
     # PPO settings
     ppo_settings = policy_params["ppo_settings"]
     model_checkpoint = ppo_settings["model_checkpoint"]
+    time_steps = ppo_settings["time_steps"]
+    seeds = ppo_settings["seeds"]
 
     # Load wrappers settings as dictionary
     wrappers_settings = load_settings_flat_dict(WrappersSettings, settings_params["wrappers_settings"])
+    stack_frames = settings_params["wrappers_settings"]["stack_frames"]
     # Load shared settings
     settings = settings_params["settings"]["shared"]
     # Set action space type
     settings["action_space"] = SpaceTypes.DISCRETE if settings["action_space"] == "discrete" else SpaceTypes.MULTI_DISCRETE
+    settings["action_space"] = (settings["action_space"], settings["action_space"])
 
-    envs_settings = []
     # Load game specific settings
-    if train_id:
-        game_settings = settings_params["settings"][train_id]
-        if char_transfer:
-            for character in game_settings["characters"]:
-                game_settings["characters"] = character
-                env_settings = settings.copy()
-                env_settings.update(game_settings)
-                env_settings = load_settings_flat_dict(EnvironmentSettings, env_settings)
-                envs_settings.append(env_settings)
-        else:
-            game_settings["characters"] = game_settings["characters"][0]
-            env_settings = settings.copy()
-            env_settings.update(game_settings)
-            env_settings = load_settings_flat_dict(EnvironmentSettings, env_settings)
-            envs_settings.append(env_settings)
-    else:
-        for game_id in game_ids:
-            game_settings = settings_params["settings"][game_id]
-            game_settings["characters"] = game_settings["characters"][0]
-            env_settings = settings.copy()
-            env_settings.update(game_settings)
-            env_settings = load_settings_flat_dict(EnvironmentSettings, env_settings)
-            envs_settings.append(env_settings)
+    game_settings = settings_params["settings"][train_id]
+    game_settings["characters"] = (game_settings["characters"][0], game_settings["characters"][0])
+    settings.update(game_settings)
+    settings = load_settings_flat_dict(EnvironmentSettingsMultiAgent, settings)
 
+    envs_settings = [settings]
+    register_env("DiambraArena", env_creator)
     eval_results = {}
     for seed in seeds:
         eval_results.update({seed: {}})
@@ -103,34 +98,50 @@ def main(policy_cfg: str, settings_cfg: str, train_id: str | None, char_transfer
             epoch_settings = envs_settings[epoch]
             config = {
                 # Define and configure the environment
-                "env": DiambraArena,
+                "env": "DiambraArena",
                 "env_config": {
                     "game_id": epoch_settings.game_id,
                     "settings": epoch_settings,
                     "wrappers_settings": wrappers_settings,
                 },
-                "num_workers": 0,
-                "train_batch_size": 200,
+                "num_workers": 3,
+                "num_envs_per_worker": 2,
+                "train_batch_size": time_steps,
                 "framework": "torch",
+                # Evaluate once per two training iterations
+                "evaluation_interval": 2,
+                # Run evaluation on (at least) one episode
+                "evaluation_duration": 1,
+                "evaluation_num_workers": 1,
+                "evaluation_config": {
+                    "render_env": False,
+                },
             }
             # Update config file
             config = preprocess_ray_config(config)
-
+            
             # Create the RLlib Agent.
             agent = PPO(config=config)
-            print("Policy architecture =\n{}".format(agent.get_policy().model))
-            print("Training agent...")
-            results = agent.train()
-            print("\n .. training completed.")
-            print("Training results:\n{}".format(pretty_print(results)))
+            # print("Policy architecture =\n{}".format(agent.get_policy().model))
+            
+            for i in range(1, 2):
+                print("Epoch {}".format(i))
+                print("Training agent...")
+                results = agent.train()
+                print("\n .. training completed.")
+                print("Training results:\n{}".format(pretty_print(results)))
 
-            checkpoint = agent.save().checkpoint.path
-            print("Checkpoint saved at {}".format(checkpoint))
-
-            print("\nStarting evaluation ...\n")
-            results = agent.evaluate()
-            print("\n... evaluation completed.\n")
-            print("Evaluation results:\n{}".format(pretty_print(results)))
+            # Save agent
+            model_checkpoint = str(int(model_checkpoint) + time_steps)
+            model_path = os.path.join(model_folder, f"seed_{seed}", model_checkpoint)
+            agent.save(model_path)
+            print("Agent saved at {}".format(model_path))
+            
+            # # Evaluate
+            # print("\nStarting evaluation ...\n")
+            # results = agent.evaluate()
+            # print("\n... evaluation completed.\n")
+            # print("Evaluation results:\n{}".format(pretty_print(results)))
 
             return 0
 
@@ -138,8 +149,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--policyCfg", type=str, required=True, help="Policy config")
     parser.add_argument("--settingsCfg", type=str, required=True, help="Env settings config")
-    parser.add_argument("--trainID", type=str, required=False, help="Specific game to train on")
-    parser.add_argument('--charTransfer', type=int, required=True, help="Evaluate character transfer or not")
+    parser.add_argument("--trainID", type=str, required=True, help="Specific game to train on")
     opt = parser.parse_args()
 
-    main(opt.policyCfg, opt.settingsCfg, opt.trainID, bool(opt.charTransfer))
+    main(opt.policyCfg, opt.settingsCfg, opt.trainID)
